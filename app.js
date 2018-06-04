6a77725793004a9de672e48d5d1408f79309e11b
Yumar Sotolongo Rivero <yumarsoto19831@gmail.com>
2018-06-04 14:28:09 -0400

    First implementation approach

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ const wordListPath = require("word-list");
 const app = express();
 const wordList = fs.readFileSync(wordListPath, "utf8").split("\n");
 
+let wordListAnagram = {};
+
+function processWorldList() {
+  wordList.map(x => {
+    let sort = sortString(x);
+    if (wordListAnagram[sort]) wordListAnagram[sort].push(x);
+    else wordListAnagram[sort] = Array(x);
+  });
+}
+
 app.get("/ping", (req, res) => res.send("pong"));
 
 app.get("/", (req, res) => res.send("Hello guys from SweetIQ"));
@@ -29,7 +39,11 @@ app.get("/", (req, res) => res.send("Hello guys from SweetIQ"));
  *   ]
  */
 app.get("/find", (req, res) => {
-  res.json([wordList[0], wordList[1]]);
+  res.json(
+    wordListAnagram[sortString(req.query.word)].filter(
+      x => x != req.query.word.toLowerCase()
+    )
+  );
 });
 
 /**
@@ -49,7 +63,25 @@ app.get("/find", (req, res) => {
  *   false
  */
 app.get("/compare", (req, res) => {
-  res.json(false);
+  res.send(anagram(req.query.word1, req.query.word2));
 });
 
-app.listen(3001, () => console.log("App listening on port 3001"));
+function anagram(str1, str2) {
+  let isAnagram = false;
+  if (str1.lenght == str2.lenght) {
+    let sort1 = [...str1.toLowerCase()].sort().join("");
+    let sort2 = [...str2.toLowerCase()].sort().join("");
+    isAnagram = sort1 === sort2 ? true : false;
+  }
+  return isAnagram;
+}
+
+function sortString(str) {
+  str = str.toLowerCase();
+  return [...str].sort().join("");
+}
+
+app.listen(3001, () => {
+  console.log("App listening on port 3001");
+  processWorldList();
+});
